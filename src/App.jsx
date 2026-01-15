9622bd989c05e2352505621865f588df6fdae3f4
Roman Meinl <roman-meinl@ukr.net>
2026-01-15 13:57:51 +0200

    create styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
+import { useState } from "react";
+import "./App.css";
 
-import { useState } from 'react';
-import './App.css';
+const NameInput = () => {
+  const [name, setName] = useState("");
 
-function App() {
-  const [name , setName] = useState("");
+  return (
+    <div className="name-container">
+      <h1 className="name-heading">
+        Your name is: <span className="name-span">{name}</span>
+      </h1>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => {
+          const value = e.target.value;
+          const formatted =
+            value.charAt(0).toLocaleUpperCase() + value.slice(1);
+          setName(formatted);
+        }}
+        placeholder="Enter your name"
+        className="name-input"
+      />
+      <button
+        className="name-button"
+        onClick={() => setName("")}
+      >
+        Clear
+      </button>
+    </div>
+  );
+};
 
- return (
-  <>
-  <h1 style={{color:"red"}}>Your name is : <span style={{color:"black"}}>{name}</span></h1>
-  <input 
-  type="text" 
-  onChange={(e) => {
-   const value = e.target.value;
-   const formatted = value.charAt(0).toLocaleUpperCase() + value.slice(1);
-   setName(formatted)
-  }
-
-  }
-  placeholder='Enter your name'
-
-  />
-  <button onClick={() => setName("")}>Clear</button>
-  </>
- )
-}
-
-export default App;
+export default NameInput;
